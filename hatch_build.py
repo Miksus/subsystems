f36ef277175a455b952dabb99978a3d9f17e2ee6
Mikael Koli <koli.mikael@gmail.com>
2022-12-23 20:47:02 +0200

    pkg: updated npm build

--- hatch_build.py
+++ hatch_build.py
@@ -1,20 +1,93 @@
+from typing import Union
+from dataclasses import dataclass
+from shlex import shlex
+from pathlib import Path
 import shutil
 import subprocess
 import os
+import logging
+from typing import List
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
-from hatch_jupyter_builder import npm_builder
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(logging.StreamHandler())
 
-def build_app(target_name, *args, **kwargs):
-    npm_builder(target_name=target_name, *args, **kwargs, source_dir="/app")
+def run_cmd(cmd, **kwargs):
+    kwargs.setdefault("shell", os.name == "nt")
+    subprocess.check_call(cmd, **kwargs)
 
-def build_react():
-    os.chdir("frontend")
-    subprocess.call(["npm.cmd", "run",  "build"])
-    os.chdir("..")
+def get_program(prog:str) -> str:
+    if not Path(prog).is_absolute():
+        # If a command is not an absolute path find it first.
+        path = shutil.which(prog)
+        if not path:
+            raise ValueError(f"Cannot find program: {prog}")
+        return path
+    return prog
+
+def build_npm(path=".", npm=None, build_script="build"):
+    path = Path(path).resolve()
+
+    # Find a suitable default for the npm command.
+    if npm is None:
+        use_yarn = (path / "yarn.lock").exists()
+        if use_yarn and not shutil.which("yarn"):
+            use_yarn = False
+
+        if use_yarn:
+            npm = "yarn"
+        else:
+            npm = "npm"
+
+    prog = get_program(npm)
+
+    cmd_install = [prog, "install"]
+    cmd_build = [prog, "run", build_script]
+
+    LOGGER.info(f"Installing dependencies: {cmd_install}")
+    run_cmd(cmd_install, cwd=path)
+    LOGGER.info(f"Building with: {cmd_build}")
+    run_cmd(cmd_build, cwd=path)
+
+def copy_build(source, target):
+    LOGGER.info(f"Copying {source} to {target}")
+    shutil.rmtree(target, ignore_errors=True)
+    shutil.copytree(source, target)
+
+def target_exists(dir):
+    path = Path(dir)
+    return any(Path(path).iterdir())
+
+
+@dataclass
+class BuildConfig:
+    source: str = "."
+    target: str = None
+    build: str = "build"
+    skip_exist: bool = False
+
 
 class CustomHook(BuildHookInterface):
 
-    def initialize(self, *args, **kwargs):
-        shutil.rmtree("subsystems/app", ignore_errors=True)
-        shutil.copytree("app/build", "subsystems/app")
+    def initialize(self, version, build_data):
+        config = BuildConfig(**self.config)
+
+        source_dir = Path(config.source).resolve()
+        build_dir = source_dir / config.build
+        target_dir = Path(config.target).resolve()
+
+        LOGGER.info(f"Running hook with {config}")
+        if os.getenv("SKIP_HATCH_NPM_BUILDER"):
+            LOGGER.info("Builder ignored. Skipping.")
+            return
+        if target_exists(target_dir) and config.skip_exist:
+            LOGGER.info("Target exists. Skipping.")
+            return
+
+        build_npm(path=source_dir)
+
+        if target_dir is not None:
+            copy_build(build_dir, target_dir)
+
+        LOGGER.info(f"Build done")
