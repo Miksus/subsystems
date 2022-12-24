9cbd01b73646ed72a1b1769c3748b2e9dbbb5ef0
Mikael Koli <koli.mikael@gmail.com>
2022-12-24 21:45:17 +0200

    test: fix config tests

--- subsystems/tests/test_config.py
+++ subsystems/tests/test_config.py
@@ -33,12 +33,13 @@ def test_fastapi():
 
     serv = sys["backend"]
     assert isinstance(serv, ServerBase)
-    assert isinstance(serv.app, FastAPI)
-    assert isinstance(serv.server, uvicorn.Server)
+    assert isinstance(serv.app_instance, FastAPI)
+    assert isinstance(serv.instance, uvicorn.Server)
 
-    assert serv.server.config.app is serv.app
-    assert serv.server.config.app.description == "My app"
-    assert serv.server.config.workers == 1
+    config = serv.instance.config
+    assert config.app is serv.app_instance
+    assert config.app.description == "My app"
+    assert config.workers == 1
 
 def test_custom_fastapi(tmpdir, request):
     orig_paths = sys.path.copy()
@@ -70,9 +71,9 @@ def test_custom_fastapi(tmpdir, request):
 
         serv = systems["backend"]
         assert isinstance(serv, ServerBase)
-        assert isinstance(serv.app, FastAPI)
-        assert isinstance(serv.server, uvicorn.Server)
-        assert serv.server.config.app.description == "Myapplication"
+        assert isinstance(serv.app_instance, FastAPI)
+        assert isinstance(serv.instance, uvicorn.Server)
+        assert serv.instance.config.app.description == "Myapplication"
     finally:
         sys.path = orig_paths
 
@@ -103,7 +104,7 @@ def test_static_app(tmpdir):
         )
         assert isinstance(systems, Subsystems)
 
-        app = systems["backend"].app
+        app = systems["backend"].app_instance
         assert app.description == "My app"
 
         client = TestClient(app)
@@ -135,68 +136,12 @@ def test_scheduler(tmpdir, request):
         )
         assert isinstance(systems, Subsystems)
 
-        app = systems["backend"].app
+        app = systems["backend"].app_instance
         assert isinstance(app, Rocketry)
         assert app.session.config.execution == "main"
     finally:
         sys.path = orig_paths
 
-def test_cluster(tmpdir, request):
-    orig_paths = sys.path.copy()
-    test_name = request.node.originalname
-    module_sched = f"config_{test_name}_sched"
-    module_api = f"config_{test_name}_api"
-    try:
-        sys.path.append(str(tmpdir))
-        app_dir = tmpdir.mkdir(f"mydir")
-
-        file = app_dir.join(f"{module_sched}.py")
-        file.write(dedent("""
-        from rocketry import Rocketry
-        myapp = Rocketry(execution="main")
-        """))
-
-        file = app_dir.join(f"{module_api}.py")
-        file.write(dedent("""
-        from fastapi import FastAPI
-        myapp = FastAPI()
-        """))
-
-        systems = Config.parse(
-            {
-                "apps": {
-                    "backend": {
-                        "app": {
-                            "type": "subsystems.apps.ClusterApp",
-                            "api": {
-                                "app": {
-                                    "instance": f"mydir.{module_api}:myapp"
-                                },
-                                "server": {
-                                    "type": "uvicorn.Server",
-                                }
-                            },
-                            "scheduler": {
-                                "app": {
-                                    "instance": f"mydir.{module_sched}:myapp"
-                                },
-                            },
-                        }
-                    }
-                }
-            }
-        )
-        assert isinstance(systems, Subsystems)
-
-        backend = systems["backend"]
-        assert backend.server.apps.keys() == {"scheduler", "api"}
-        assert isinstance(backend.server.apps["api"].app, FastAPI)
-        assert isinstance(backend.server.apps["scheduler"].app, Rocketry)
-        #app = systems["backend"].app
-        #assert isinstance(app, Rocketry)
-        #assert app.session.config.execution == "main"
-    finally:
-        sys.path = orig_paths
 
 @pytest.mark.parametrize("server,app,server_cls,app_cls", [
     pytest.param("uvicorn", "FastAPI", "uvicorn.Server", "fastapi.FastAPI", id="uvicorn (FastAPI)"), 
